f6e192ca5f2375820dc1c71cfdfee2f3eda56fd8
zhangyuan <zhangyuan@mockuai.com>
2016-03-11 10:29:28 +0800

    Initial Commit

---
+++ test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by zhangyuan on 16/3/11.
+//  Copyright © 2016年 zhangyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by zhangyuan on 16/3/11.
+//  Copyright © 2016年 zhangyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
